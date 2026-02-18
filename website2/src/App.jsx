d8db6213ff542f7c1aa315e8d42d5abdc8a435b2
Eric Yang <eric_y@mit.edu>
2026-02-18 12:56:21 -0500

    display sessions for day

--- website2/src/App.jsx
+++ website2/src/App.jsx
@@ -7,42 +7,71 @@ const online_instruments_url = "http://octavio-server.mit.edu:5001/api/online_in
 const log_url = "http://octavio-server.mit.edu:5001/api/logs"
 const test_url = "http://octavio-server.mit.edu:5001/"
 
-async function fetchOnlineInstruments() {
-  const response = await fetch(online_instruments_url)
-  return response.text()
-}
+// async function fetchOnlineInstruments() {
+//   const response = await fetch(online_instruments_url)
+//   return response.text()
+// }
 
-async function fetchLog(date) {
-  const params = {
-    date: date.toISOString().split('T')[0]
-  };
-  const url = new URL(log_url);
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-  const response = await fetch(url)
-  return response.text()
-}
+// async function fetchLog(date) {
+//   const params = {
+//     date: date.toISOString().split('T')[0]
+//   };
+//   const url = new URL(log_url);
+//   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+//   const response = await fetch(url)
+//   return response.text()
+// }
 
 function App() {
   const [online_instruments, setOnlineInstruments] = useState([])
   const [date, setDate] = useState(new Date())
+  const [session_ids, setSessionIds] = useState(new Set())
   const [log_info, setLogInfo] = useState("")
 
+  async function fetchOnlineInstruments() {
+    const response = await fetch(online_instruments_url)
+    setOnlineInstruments(await response.text())
+  }
+
+  async function fetchLog() {
+    const params = {
+      date: date.toISOString().split('T')[0]
+    };
+    const url = new URL(log_url);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    const response = await fetch(url)
+    const response_text = await response.text();
+    const response_json = JSON.parse(response_text);
+    const new_session_ids = new Set(response_json.filter(
+      (item) => item['operation'] === 'ADD_CHUNK'
+    ).map(
+      (item) => item['session_id']
+    ));
+    setLogInfo(response_text);
+    setSessionIds(new_session_ids);
+  }
+
+  function init() {
+    fetchOnlineInstruments();
+    fetchLog();
+  }
+
   useEffect(() => {
-    fetchOnlineInstruments().then(data => setOnlineInstruments(data));
-    const intervalId = setInterval(() => {
-      fetchOnlineInstruments().then(data => setOnlineInstruments(data))
-    }, 30 * 1000) // 30 seconds
+    init();
+    const intervalId = setInterval(fetchOnlineInstruments, 30 * 1000) // 30 seconds
     return () => clearInterval(intervalId);
   }, [])
+
   return (
     <>
       <h1>Octavio Website</h1>
       <h2>Online Instruments: {online_instruments}</h2>
       <div className="logs">
         <DatePicker showIcon selected={date} onChange={(date) => {
-          setDate(date)
-          fetchLog(date).then(data=>setLogInfo(data))
+          setDate(date);
+          fetchLog();
         }}/>
+        <div className="session-ids">{'Sessions: {' + Array.from(session_ids).join(', ') + '}'}</div>
         <div className="log-body">{log_info}</div>
       </div>
     </>
