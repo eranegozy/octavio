911d6d37b6db901e5c96098bb5a92593352e9582
Ethan Liu <ethan.yh.liu@gmail.com>
2026-07-26 15:24:57 -0400

    add Transkun AMT implementation and TODO testing flags

--- client/amt.py
+++ client/amt.py
@@ -1,16 +1,14 @@
 from abc import ABC, abstractmethod
 
 class AMTModel(ABC):
-
     @abstractmethod
     def transcribe(self, input_audio_path: str, output_midi_path: str):
         pass
 
-    @staticmethod
-    def create(model_type: str) -> "AMTModel":
-        if model_type == "basic_pitch":
-            from amt_basic_pitch import BasicPitchAMT
-            return BasicPitchAMT()
-        if model_type == "transkun":
-            from amt_transkun import TranskunAMT
-            return TranskunAMT()
+def get_amt_model(model_type: str) -> "AMTModel":
+    if model_type == "basic_pitch":
+        from amt_basic_pitch import BasicPitchAMT
+        return BasicPitchAMT()
+    if model_type == "transkun":
+        from amt_transkun import TranskunAMT
+        return TranskunAMT()
--- client/amt_transkun.py
+++ client/amt_transkun.py
@@ -1,5 +1,39 @@
-import transkun.transcribe as tk
+from amt import AMTModel
+
+import pkg_resources
+import torch
+import moduleconf
+import soxr
+
+import logging
+
+from transkun.transcribe import readAudio
+from transkun.Data import writeMidi
 
 class TranskunAMT(AMTModel):
-    def transcribe(self, input_audio_path, output_midi_path):
-        raise NotImplementedError
+    # TODO: TEST IF THIS WORKS
+    def __init__(self, device="cpu", weight_path=None, conf_path=None):
+        weight_path = weight_path or pkg_resources.resource_filename("transkun", "pretrained/2.0.pt")
+        conf_path = conf_path or pkg_resources.resource_filename("transkun", "pretrained/2.0.pt")
+
+        conf_manager = moduleconf.parseFromFile(conf_path)
+        transkun = conf_manager["Model"].module.TransKun
+        conf = conf_manager["Model"].config
+
+        checkpoint = torch.load(weight_path, map_location=device)
+        model = transkun(conf=conf).to(device)
+
+        key = "best_state_dict" if "best_state_dict" in checkpoint else "state_dict"
+        model.load_state_dict(checkpoint[key], strict=False)
+        model.eval()
+
+        self.model = model
+
+    def transcribe(self, input_audio_path, output_midi_path, device="cpu"):
+        fs, audio = readAudio(input_audio_path)
+        if fs != self.model.fs:
+            logging.warning(f"Sampling rate mismatch between audio {input_audio_path} and Transkun AMT Model")
+            audio = soxr.resample(audio, fs, self.model_fs)
+        x = torch.from_numpy(audio).to(device)
+        notes_estimate = self.model.transcribe(x, discardSecondHalf=False)
+        writeMidi(notes_estimate).write(output_midi_path)
